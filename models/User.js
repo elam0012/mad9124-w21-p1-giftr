002bfce009462465f0bd3360cdf60a9c6b006e17
Yasser Ehaimer <yehaimer@Yassers-MacBook-Pro.local>
2022-04-09 21:22:31 -0400

    refactored authintication

--- models/User.js
+++ models/User.js
@@ -1,10 +1,45 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 import mongoose from 'mongoose'
 
+const saltRounds = 14
+const jwtSecretKey = 'superSecureSecret'
+
 const schema = new mongoose.Schema({
-  firstName: { type: String, trim: true, maxlength: 64, required: true },
-  lastName: { type: String, trim: true, maxlength: 64 },
-  email: { type: String, trim: true, maxlength: 512, required: true, unique: true },
-  password: { type: String, trim: true, maxlength: 70, required: true }
+  firstName: {type: String, trim: true, maxlength: 64, required: true},
+  lastName: {type: String, trim: true, maxlength: 64},
+  email: {
+    type: String,
+    trim: true,
+    maxlength: 512,
+    required: true,
+    unique: true
+  },
+  password: {type: String, trim: true, maxlength: 70, required: true}
+})
+
+schema.methods.generateAuthToken = function() {
+  const payload = {uid: this._id}
+  return jwt.sign(payload, jwtSecretKey)
+}
+
+schema.statics.authenticate = async function(email, password) {
+  const user = await this.findOne({email: email})
+
+  const badHash = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
+  // remember if the email did not match, user === null
+  const hashedPassword = user ? user.password : badHash
+  const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
+
+  return passwordDidMatch ? user : null
+}
+
+schema.pre('save', async function(next) {
+  // Only encrypt if the password property is being changed.
+  if (!this.isModified('password')) return next()
+
+  this.password = await bcrypt.hash(this.password, saltRounds)
+  next()
 })
 
 const Model = mongoose.model('User', schema) // factory function returns a class
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,17 +1,19 @@
-import express from 'express'
-import sanitizeBody from '../../middleware/sanitizeBody.js'
-import debug from 'debug'
-import User from "../../models/User.js"
-import bcrypt from 'bcrypt'
 
-const saltRounds = 14
+
+import User from '../../models/User.js'
+import sanitizeBody from '../../middleware/sanitizeBody.js'
+import createDebug from 'debug'
+import express from 'express'
+
+const debug = createDebug('week8:auth')
 const router = express.Router()
 
 // Register a new user
 router.post('/users', sanitizeBody, async (req, res) => {
   try {
-    let newUser = new User(req.sanitizedBody)
-    const itExists = Boolean(await User.countDocuments({ email: newUser.email }))
+    const newUser = new User(req.sanitizedBody)
+
+    const itExists = Boolean(await User.countDocuments({email: newUser.email}))
     if (itExists) {
       return res.status(400).send({
         errors: [
@@ -19,66 +21,45 @@ router.post('/users', sanitizeBody, async (req, res) => {
             status: '400',
             title: 'Validation Error',
             detail: `Email address '${newUser.email}' is already registered.`,
-            source: { pointer: '/data/attributes/email' }
+            source: {pointer: '/data/attributes/email'}
           }
         ]
       })
     }
-    newUser.password = await bcrypt.hash(newUser.password, saltRounds)
+
     await newUser.save()
-    res.status(201).send({ data: newUser })
+    res.status(201).send({data: newUser})
   } catch (err) {
-    debug(err)
+    debug('Error saving new user: ', err.message)
     res.status(500).send({
       errors: [
         {
           status: '500',
           title: 'Server error',
-          description: 'Problem saving document to the database.',
-        },
-      ],
+          description: 'Problem saving document to the database.'
+        }
+      ]
     })
   }
 })
 
 // Login a user and return an authentication token.
-// router.post('/tokens', sanitizeBody, async (req, res) => {
-//   const { email, password } = req.sanitizedBody
-//   const user = await User.findOne({ email: email })
-//   if (!user) {
-//     return res.status(401).send({ errors: ['we will build this later'] })
-//   }
+router.post('/tokens', sanitizeBody, async (req, res) => {
+  const {email, password} = req.sanitizedBody
+  const user = await User.authenticate(email, password)
 
-//   const hashedPassword = user.password
-//   const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
-//   if (!passwordDidMatch) {
-//     return res.status(401).send({ errors: ['we will build this later'] })
-//   }
-
-//   const token = 'iamatoken'
-//   res.status(201).send({ data: { token } })
-
-  // check if the payload.username is valid
-  // retrieve the stored password hash
-  // compare the payload.password with the hashed password
-  // if all is good, return a token
-  // if any condition failed, return an error message
-
-  router.post('/tokens', sanitizeBody, async (req, res) => {
-  const { email, password } = req.sanitizedBody
-
-  const user = await User.findOne({ email: email })
-
-  const badHash = `$2b$${saltRounds}$invalidusernameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`
-  const hashedPassword = user ? user.password : badHash
-  const passwordDidMatch = await bcrypt.compare(password, hashedPassword)
-
-  if (!user || !passwordDidMatch) {
-    return res.status(401).send({ errors: ['we will build this later'] })
+  if (!user) {
+    return res.status(401).send({
+      errors: [
+        {
+          status: '401',
+          title: 'Incorrect username or password.'
+        }
+      ]
+    })
   }
 
-  const token = 'iamatoken'
-  res.status(201).send({ data: { token } })
+  res.status(201).send({data: {token: user.generateAuthToken()}})
 })
 
 export default router
